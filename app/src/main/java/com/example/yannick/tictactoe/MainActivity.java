12903be90b5638fa993da135e34c58c32a013909
MONTES Yannick <montes.yannick@gmail.com>
2017-09-29 16:50:40 -0400

    Create game activity & links entered names

--- app/src/main/java/com/example/yannick/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/yannick/tictactoe/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.yannick.tictactoe;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +14,17 @@ public class MainActivity extends AppCompatActivity {
         Log.i("MainActivity","OnCreate");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void runGame(View view)
+    {
+        Intent gameActivity = new Intent(this, activity_game.class);
+        EditText Xedit = (EditText) findViewById(R.id.nomX);
+        EditText Yedit = (EditText) findViewById(R.id.nomO);
+        String Xplayer = Xedit.getText().toString();
+        String Oplayer = Yedit.getText().toString();
+        gameActivity.putExtra("Oplayer", Oplayer);
+        gameActivity.putExtra("Xplayer", Xplayer);
+        startActivity(gameActivity);
     }
 }
--- app/src/main/java/com/example/yannick/tictactoe/activity_game.java
+++ app/src/main/java/com/example/yannick/tictactoe/activity_game.java
@@ -1,7 +1,9 @@
 package com.example.yannick.tictactoe;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class activity_game extends AppCompatActivity {
 
@@ -9,5 +11,16 @@ public class activity_game extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        Intent gameIntent = getIntent();
+
+        String Oplayer = gameIntent.getStringExtra("Oplayer");
+        String Xplayer = gameIntent.getStringExtra("Xplayer");
+
+        TextView Oview = (TextView) findViewById(R.id.labelJO);
+        Oview.setText("0: "+Oplayer);
+
+        TextView Xview = (TextView) findViewById(R.id.labelJX);
+        Xview.setText("X: "+Xplayer);
     }
 }
